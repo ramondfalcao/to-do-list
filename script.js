96dfee17c060bc036a0b95de86e0564a325efa76
Ramond Falcão <ramondfalcao@gmail.com>
2021-12-14 14:01:31 -0300

    Requisito 7 concluido

--- script.js
+++ script.js
@@ -5,11 +5,14 @@
     const button = document.querySelector('#criar-tarefa');
 
     button.addEventListener('click' , function(){
-        const texto = input.value;    
-        console.log(texto)
+        const texto = input.value;        
         let item = document.createElement('li')
         item.innerText = texto
         document.querySelector('#lista-tarefas').appendChild(item)   
+        item.className = "item"
         input.value = ''
+        item.addEventListener('click', function(){
+            item.style.backgroundColor = 'rgb(128, 128, 128)'
+        })
     });
-
+    
