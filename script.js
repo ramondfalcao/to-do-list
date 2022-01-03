fa19d3625591e044beb24752ea3dd9804b6f2096
Ramond Falcão <ramondfalcao@gmail.com>
2022-01-03 17:29:19 -0300

    Requisito 8 e 9

--- script.js
+++ script.js
@@ -11,8 +11,31 @@
         document.querySelector('#lista-tarefas').appendChild(item)   
         item.className = "item"
         input.value = ''
-        item.addEventListener('click', function(){
-            item.style.backgroundColor = 'rgb(128, 128, 128)'
-        })
+       
     });
+
+    const gray = "rgb(128, 128, 128)"
+
+    let selecionado = ''
+    
+    let ol = document.querySelector('#lista-tarefas')
+    
+    ol.addEventListener('click', function(event){
+        let item = event.target;     
+        if (document.querySelector('.selected')) {
+            document.querySelector('.selected').classList.remove('selected')
+        } 
+        item.classList.add('selected')
+    })
+
+    ol.addEventListener('dblclick', function(event){
+        let item = event.target;     
+        if (item.classList.contains('completed')) {
+           item.classList.remove('completed')
+        } else {
+            item.classList.add('completed')
+        }
+    })
+
+
     
